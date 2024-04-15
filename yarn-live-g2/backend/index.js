074fd0b168b8b23b6bb569e22030d035ae0cd9ac
Quentin Launay <50755766+Elsword24@users.noreply.github.com>
2024-04-15 11:34:13 +0200

    update placement

--- yarn-live-g2/backend/index.js
+++ yarn-live-g2/backend/index.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 var uniqid = require('uniqid');
-const GameService = require('./services/game.service');
+const GameService = require('./services/game.services');
 
 // ---------------------------------------------------
 // -------- CONSTANTS AND GLOBAL VARIABLES -----------
