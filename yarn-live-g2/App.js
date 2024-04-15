834baa7d5e3224807f3c41a5051699e58fe2e844
Quentin Launay <50755766+Elsword24@users.noreply.github.com>
2024-04-15 11:03:54 +0200

    add controller

--- yarn-live-g2/App.js
+++ yarn-live-g2/App.js
@@ -6,6 +6,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './app/screens/home.screen.js';
 import { SocketContext, socket } from './app/contexts/socket.context';
+import OnlineGameScreen from './app/screens/online-game.screen';
+import VsBotGameScreen from './app/screens/vs-bot-game.screen';
 
 
 const Stack = createStackNavigator();
@@ -13,13 +15,15 @@ LogBox.ignoreAllLogs(true);
 
 function App() {
   return (
-  <SocketContext.Provider value={socket}>    
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="HomeScreen">
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  </SocketContext.Provider>
+    <SocketContext.Provider value={socket}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="HomeScreen">
+          <Stack.Screen name="HomeScreen" component={HomeScreen} />
+          <Stack.Screen name="OnlineGameScreen" component={OnlineGameScreen} />
+          <Stack.Screen name="VsBotGameScreen" component={VsBotGameScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SocketContext.Provider>
   );
 }
 
--- yarn-live-g2/app/screens/home.screen.js
+++ yarn-live-g2/app/screens/home.screen.js
@@ -1,71 +1,30 @@
-import { useEffect, useState } from "react";
-import { StyleSheet, Text, View, Platform } from "react-native";
-import io from "socket.io-client";
+// app/screens/home.screen.js
 
-console.log('Emulation OS Platform: ', Platform.OS);
-// Also usable : "http://10.0.2.2:3000"
-export const socketEndpoint = Platform.OS === 'web' ? "http://localhost:3000" : "http://192.168.103.134:3000";
-// '172.20.10.3' must be replaced by your intern IP adress
+import { StyleSheet, View, Button } from "react-native";
 
-export default function HomeScreen() {
-  const [hasConnection, setConnection] = useState(false);
-  const [time, setTime] = useState(null);
-
-  useEffect(function didMount() {
-    const socket = io(socketEndpoint, {
-      transports: ["websocket"],
-    });
-
-    socket.io.on("open", () => setConnection(true));
-    socket.io.on("close", () => setConnection(false));
-
-    socket.on("time-msg", (data) => {
-      setTime(new Date(data.time).toString());
-    });
-
-    return function didUnmount() {
-      socket.disconnect();
-      socket.removeAllListeners();
-    };
-  }, []);
-
+export default function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      {!hasConnection && (
-        <>
-          <Text style={styles.paragraph}>
-            Connecting to {socketEndpoint}...
-          </Text>
-          <Text style={styles.footnote}>
-            Make sure the backend is started and reachable
-          </Text>
-        </>
-      )}
-
-      {hasConnection && (
-        <>
-          <Text style={[styles.paragraph, { fontWeight: "bold" }]}>
-            Server time
-          </Text>
-          <Text style={styles.paragraph}>{time}</Text>
-        </>
-      )}
+      <View>
+        <Button
+          title="Jouer en ligne"
+          onPress={() => navigation.navigate('OnlineGameScreen')}
+        />
+      </View>
+      <View>
+        <Button
+          title="Jouer contre le bot"
+          onPress={() => navigation.navigate('VsBotGameScreen')}
+        />
+      </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
-  paragraph: {
-    fontSize: 16,
-  },
-  footnote: {
-    fontSize: 14,
-    fontStyle: "italic",
-  },
+  }
 });
